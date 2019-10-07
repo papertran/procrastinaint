cf66ccc765f3644f5154ade770e9405c95a97c29
Sean O'Meara <51218897+sean7956@users.noreply.github.com>
2019-10-07 15:15:03 -0400

    Created MainCP
    Also put in the data types that are being stored
    Implemented the functions needed

---
+++ app/src/main/java/edu/fsu/cen4020/android/procrastinaint/MainCP.java
@@ -0,0 +1,96 @@
+package edu.fsu.cen4020.android.procrastinaint;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.net.Uri;
+
+public class MainCP extends ContentProvider {
+    public final static String DBNAME = "Events";
+    public final static String TABLE_NAMESTABLE = "EventTable";
+    public static final Uri CONTENT_URI = Uri.parse("content://edu.fsu.cen4020.android.procrasinaint.MainCP.provider");
+    MainDatabaseHelper mOpenHelper;
+
+    public final static String _ID ="_ID";
+    public final static String Title = "Title";
+    public final static String StartDay = "StartDay";
+    public final static String EndDay = "EndDay";
+    public final static String StartDate = "StartDate";
+    public final static String EndDate = "EndDate";
+    public final static String Location = "Location";
+    public final static String Notes = "Notes";
+    public final static String Invitee = "Invitee";
+
+    private static final String SQL_CREATE_MAIN =
+            "CREATE TABLE " + TABLE_NAMESTABLE+" ( "+
+                    " _ID INTEGER PRIMARY KEY, " +
+                    "Title TEXT, " +
+                    "StartDay TEXT, " +
+                    "EndDay TEXT," +
+                    "StartDate TEXT," +
+                    "EndDate TEXT," +
+                    "Location TEXT," +
+                    "Notes TEXT," +
+                    "Invitee TEXT)";
+
+    protected static final class MainDatabaseHelper extends SQLiteOpenHelper {
+        MainDatabaseHelper(Context context){
+            super(context, DBNAME,null,1);
+        }
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            db.execSQL(SQL_CREATE_MAIN);
+        }
+        @Override
+        public void onUpgrade(SQLiteDatabase arg0, int arg1, int arg2) {}
+    }
+
+    public MainCP() {
+    }
+
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        return  mOpenHelper
+                .getWritableDatabase()
+                .delete(TABLE_NAMESTABLE, selection, selectionArgs);
+    }
+
+    @Override
+    public String getType(Uri uri) {
+        return null;
+    }
+
+    @Override
+    public Uri insert(Uri uri, ContentValues values) {
+        long id = mOpenHelper
+                .getWritableDatabase()
+                .insert(TABLE_NAMESTABLE, null, values);
+        return Uri.withAppendedPath(CONTENT_URI, ""+id);
+    }
+
+    @Override
+    public boolean onCreate() {
+        mOpenHelper = new MainDatabaseHelper(getContext());
+        return true;
+    }
+
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection,
+                        String[] selectionArgs, String sortOrder) {
+        return mOpenHelper
+                .getReadableDatabase()
+                .query(TABLE_NAMESTABLE, projection, selection, selectionArgs,
+                        null, null, sortOrder);
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues values, String selection,
+                      String[] selectionArgs) {
+        return mOpenHelper
+                .getWritableDatabase()
+                .update(TABLE_NAMESTABLE, values, selection, selectionArgs);
+    }
+}
