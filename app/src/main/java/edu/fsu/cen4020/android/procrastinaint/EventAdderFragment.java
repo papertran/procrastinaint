f6ffa88f1a7e1adcf67c1468569fc6f3ad9bbbe0
Sean O'Meara <51218897+sean7956@users.noreply.github.com>
2019-10-16 09:50:29 -0400

    Worked on event_adder.xml
    Added the tile edit text
    Added the reoccurring event checkbox
    Made eventButton go to event_adder.xml

--- app/src/main/java/edu/fsu/cen4020/android/procrastinaint/EventAdderFragment.java
+++ app/src/main/java/edu/fsu/cen4020/android/procrastinaint/EventAdderFragment.java
@@ -24,7 +24,7 @@ public class EventAdderFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_event_adder, container, false);
+        return inflater.inflate(R.layout.event_adder, container, false);
 
     }
 
