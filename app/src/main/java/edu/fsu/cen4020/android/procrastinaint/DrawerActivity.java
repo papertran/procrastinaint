7f9d04e899eade544a574bd0cd55ce3881c68db1
Sean Bemiller <yuhsredux@gmail.com>
2019-11-14 14:46:51 -0500

    Updated DrawerActivity to Match XML

--- app/src/main/java/edu/fsu/cen4020/android/procrastinaint/DrawerActivity.java
+++ app/src/main/java/edu/fsu/cen4020/android/procrastinaint/DrawerActivity.java
@@ -44,8 +44,8 @@ public class DrawerActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow,
-                R.id.nav_tools, R.id.nav_share, R.id.nav_send)
+                R.id.nav_home, R.id.nav_pomodoro_timer, R.id.nav_backupToCloud,
+                R.id.nav_extras, R.id.nav_share, R.id.nav_export)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
