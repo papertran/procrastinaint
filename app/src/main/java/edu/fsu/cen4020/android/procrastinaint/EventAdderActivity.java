c9c5834ba6ad594b9e08e7ecd48ae1d50982ba55
Sean O'Meara <51218897+sean7956@users.noreply.github.com>
2019-11-14 18:18:11 -0500

    Added the TimePickerDialog
    Created the TimePickerDialog to get the time and saves it
    Works for start and end times

--- app/src/main/java/edu/fsu/cen4020/android/procrastinaint/EventAdderActivity.java
+++ app/src/main/java/edu/fsu/cen4020/android/procrastinaint/EventAdderActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.DialogFragment;
 
 import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.content.ContentValues;
 import android.database.Cursor;
 import android.os.Bundle;
@@ -11,27 +13,58 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import java.text.DateFormat;
 import java.util.Calendar;
 
-public class EventAdderActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
+public class EventAdderActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener {
 
     private String TAG = EventAdderActivity.class.getCanonicalName();
-    private String CdateSolo;
+    private String CdateSolo="";   //This contains the date of the solo date of the event
+    private String StartTimeHour="";
+    private String StartTimeMin="";
+    private String EndTimeHour="";
+    private String EndTimeMin="";
+    private int datepick = 0;   // This is used to determine which date picker was last clicked
+    private int start_or_end = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_adder);
 
+        // This button is for the Solo date picker
         final Button datePicker = (Button) findViewById(R.id.Date_picker_nonreoccurring);
         datePicker.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                datepick = 0;
                 DialogFragment date = new DatePickerFragment();
-                date.show(getSupportFragmentManager(), "date pciker");
+                date.show(getSupportFragmentManager(), "date picker");
+            }
+        });
+
+        Button startButton = (Button) findViewById(R.id.start_time);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                start_or_end = 0;
+                DialogFragment startTimePicker = new TimePickerFragment();
+                startTimePicker.show(getSupportFragmentManager(), "time picker");
+
+            }
+        });
+
+        Button endButton = (Button) findViewById(R.id.end_time);
+        endButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                start_or_end = 1;
+                DialogFragment startTimePicker = new TimePickerFragment();
+                startTimePicker.show(getSupportFragmentManager(), "time picker");
+
             }
         });
 
@@ -47,6 +80,25 @@ public class EventAdderActivity extends AppCompatActivity implements DatePickerD
 
         CdateSolo = currentDateString;
         Toast.makeText(getApplicationContext(), CdateSolo, Toast.LENGTH_LONG).show();
+
+    }
+
+    @Override
+    public void onTimeSet(TimePicker timePicker, int hour, int minute) {
+
+        if (start_or_end == 0) {
+            StartTimeHour = Integer.toString(hour);
+            StartTimeMin = Integer.toString(minute);
+            Toast.makeText(getApplicationContext(), "Start Time " + StartTimeHour + ":" + StartTimeMin, Toast.LENGTH_LONG).show();
+        }
+
+        else{
+            EndTimeHour= Integer.toString(hour);
+            EndTimeMin= Integer.toString(minute);
+            Toast.makeText(getApplicationContext(), "End Time " + EndTimeHour + ":" + EndTimeMin, Toast.LENGTH_LONG).show();
+
+        }
+
 
     }
 }
