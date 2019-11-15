3c84de9d2b6f52522e896dead06d7cbac4f19c2b
seanbemiller <yuhsredux@gmail.com>
2019-11-14 19:22:50 -0500

    UI Improvements
    Fixed slide out menu button to open slide out menu.
    Removed icon and added placeholder text from slide menu header.
    Added temp button to go to calendar view.

--- app/src/main/java/edu/fsu/cen4020/android/procrastinaint/calendar.java
+++ app/src/main/java/edu/fsu/cen4020/android/procrastinaint/calendar.java
@@ -2,8 +2,10 @@ package edu.fsu.cen4020.android.procrastinaint;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CalendarView;
+import android.widget.ImageButton;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,21 +13,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class calendar extends AppCompatActivity {
 
+    //calendar activity referenced from youtube video
     private static final String TAG = "CalendarActivity";
-
     private CalendarView my_calendarView;
+    private ImageButton menuButton;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.calendar_layout);
         my_calendarView = findViewById(R.id.calendarView);
-
+        menuButton = (ImageButton) findViewById(R.id.menuButton);
         my_calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override // i = year, i1 = month, i2 = day
             public void onSelectedDayChange(@NonNull CalendarView calendarView, int i, int i1, int i2) {
 
 
+            }
+        });
+
+        menuButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(calendar.this, DrawerActivity.class);
+                startActivity(intent);
             }
         });
     }
