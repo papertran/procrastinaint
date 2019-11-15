92580a453886c6dcc1e9f7f1508651345fc994c6
Andres Ponciano <40023209+AndresPonciano@users.noreply.github.com>
2019-11-14 22:01:15 -0500

    Did break timer for pomodoro method.

--- app/src/main/java/edu/fsu/cen4020/android/procrastinaint/timerActivity.java
+++ app/src/main/java/edu/fsu/cen4020/android/procrastinaint/timerActivity.java
@@ -14,12 +14,17 @@ import java.util.Locale;
 public class timerActivity extends AppCompatActivity {
 
     private static final long START_MILLIS = 1500000;
+//    private static final long START_MILLIS = 3000;
     private CountDownTimer mCountDownTimer;
     private boolean mTimerRunning;
     private long mTimeLeft = START_MILLIS;
+    private long breakTime = 300000;
+//    private long breakTime = 5000;
+    private long breakTimeLeft = breakTime;
 
     public int counter = 25;
     Button pomodoroButton;
+    Button breakButton;
     TextView textView;
 
     @Override
@@ -27,12 +32,21 @@ public class timerActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timer);
         pomodoroButton= (Button) findViewById(R.id.pomodoroButton);
+        breakButton = (Button) findViewById(R.id.breakButton);
+        breakButton.setVisibility(View.INVISIBLE);
         textView= (TextView) findViewById(R.id.textView);
         pomodoroButton.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View v) {
                 startTimer();
+            }
+        });
+
+        breakButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startBreak();
             }
         });
     }
@@ -42,20 +56,44 @@ public class timerActivity extends AppCompatActivity {
             @Override
             public void onTick(long millisUntilFinished) {
                 mTimeLeft = millisUntilFinished;
-                updateCountDownText();                }
+                updateCountDownText(mTimeLeft);                }
 
             @Override
             public void onFinish() {
-
+                textView.setText("You have stayed focused for 25 minutes!");
+                pomodoroButton.setVisibility(View.INVISIBLE);
+                breakButton.setVisibility(View.VISIBLE);
+                mTimeLeft = START_MILLIS;
+//                startBreak();
             }
         }.start();
 
         mTimerRunning = true;
     }
 
-    private void updateCountDownText(){
-        int minutes = (int) (mTimeLeft / 1000) / 60;
-        int seconds = (int) (mTimeLeft / 1000) % 60;
+    private void startBreak(){
+        mCountDownTimer = new CountDownTimer(breakTimeLeft, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                breakTimeLeft = millisUntilFinished;
+                updateCountDownText(breakTimeLeft);
+            }
+
+            @Override
+            public void onFinish() {
+                textView.setText("5 minute break is done. Press button to start again.");
+                pomodoroButton.setVisibility(View.VISIBLE);
+                breakButton.setVisibility(View.INVISIBLE);
+                breakTimeLeft = breakTime;
+
+            }
+        }.start();
+
+    }
+
+    private void updateCountDownText(long time){
+        int minutes = (int) (time / 1000) / 60;
+        int seconds = (int) (time / 1000) % 60;
 
         String timeLeft = String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds);
         textView.setText(timeLeft);
